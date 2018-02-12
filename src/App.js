bb11e7115909b69915b96b4941263954d311c265
Klaaz0r <klaas.foppen@gmail.com>
2018-02-12 13:40:36 +0100

    eslint and beautify

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@components/button';
+
+const App = () => (
+    <div>
+       <p>hello world!!</p>
+       <Button />
+    </div>
+);
+
+export default App;
---
+++ src/components/button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = () => (
+  <button>click me!</button>
+);
+
+export default Button;

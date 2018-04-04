bdefe6ca750bb88e33a7df8614bcddfcb8fb709a
glassfunday <klaas.foppen@gmail.com>
2018-04-04 13:09:35 +0200

    fix tags

--- src/helpers/category.js
+++ src/helpers/category.js
@@ -2,13 +2,13 @@
 
  const getCategory = tag => {
    switch (tag) {
-     case 'keycaps':
+     case 'Keycaps':
        return (<strong className="d-inline-block mb-2 text-primary">Keycaps</strong>);
-     case 'keyboard':
+     case 'Keyboard':
        return (<strong className="d-inline-block mb-2 text-success">Keyboard</strong>);
-     case 'parts':
+     case 'Parts':
        return (<strong className="d-inline-block mb-2 text-warning">Parts</strong>);
-     case 'artisan':
+     case 'Artisan':
        return (<strong className="d-inline-block mb-2 text-danger">Artisan</strong>);
      default:
        return (<strong className="d-inline-block mb-2">{tag}</strong>);

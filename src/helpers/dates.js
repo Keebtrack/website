b353681f8527d96acacefdeedd820614cc95507b
glassfunday <klaas.foppen@gmail.com>
2018-04-26 18:56:11 +0200

    change date to US

--- src/helpers/dates.js
+++ src/helpers/dates.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const dateToString = d => `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
+const dateToString = d => `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
 
 const getDates = (from, to) =>
   (<div className="mb-1 text-muted">{dateToString(new Date(from))} - {dateToString(new Date(to))}</div>);

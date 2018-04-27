7b7af0d67e1e1c880105b8e26329d624716acaef
glassfunday <klaas.foppen@gmail.com>
2018-04-27 21:12:44 +0200

    margin

--- src/components/VendorCard.js
+++ src/components/VendorCard.js
@@ -9,14 +9,13 @@ const VendorCard = ({ vendor }) => (
   <div className="col-sm-6 col-md-4 col-lg-3">
     <div
         onClick={ () => window.open(`${vendor.url}`, '_blank') }
-        className="card"
+        className="card vendor-card"
     >
       <img className="card-img-top" src={ vendor.logoUrl } alt={ vendor.name } />
       <div className="card-body">
         <h5 className="card-title">{vendor.name}</h5>
         {region(vendor.region)}
-        <p className="card-text">{`${take(100, vendor.description)}..`}.</p>
-        {/* <p className="card-text"><small className="text-muted">{vendor.url}</small></p> */}
+        <p className="card-text">{`${take(80, vendor.description)}..`}.</p>
       </div>
     </div>
   </div>
